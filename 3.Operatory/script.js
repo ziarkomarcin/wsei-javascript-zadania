7487e77d6999a8902f03dae83f770a50fb17856d
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-10-04 17:37:46 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,44 @@
+// Z A D A N I E  1
+
+let a = true;
+let b = false;
+console.log(a==b);
+
+// Z A D A N I E  2
+
+let x = 5;
+let y = 10;
+let moduloResult = 0;
+moduloResult = x%y;
+console.log(moduloResult);
+
+// Z A D A N I E  3
+
+let string1 = "Lorem";
+let string2 = "ipsum";
+
+let stringResult = string1 + " " + string2;
+
+console.log(stringResult);
+
+// Z A D A N I E  4
+
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString); // true - porównanie wartości
+console.log(someNumber === someString); // false - porównanie typów
+
+// Z A D A N I E  5
+
+let counter = 30;
+console.log(counter++);
+console.log(counter--);
+
+// Z A D A N I E  6
+
+let num1 = 10;
+let num2 = 12;
+let result = null;
+result = num1>num2;
+console.log(result);
