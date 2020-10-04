5b88d6f2f02ad384ac327064e5ec67d19830b907
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-10-04 16:44:34 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,24 @@
+//Zadanie 1
+
+let a = 6;
+let imie = "Marcin";
+let b = 2 + "dwa";
+let flag = true ;
+let c = null;
+
+console.log(a);
+console.log(imie);
+console.log(b);
+console.log(flag);
+console.log(c);
+
+//Zadanie 2
+const a = 2;
+const b = 6;
+let sum = 0;
+sum = a+b;
+console.log(sum);
+
+//Zadanie 3
+let d;
+console.log(d);//undefined, ponieważ nie ma przypisanej wartości.
