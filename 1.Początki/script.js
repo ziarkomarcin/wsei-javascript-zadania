522ffe754c3b16dfa4086dcc39e415236c78ae06
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-10-04 17:39:23 +0200

    done

--- 1.Początki/script.js
+++ 1.Początki/script.js
@@ -1,12 +1,12 @@
-//Zadanie 1:
+// Z A D A N I E  1
 console.log("Marcin");
 
-//Zadanie 2
+// Z A D A N I E  2
 console.log(12+32);
 console.log(52/16); // zmiennoprzecinkowa
 console.log(5*2);
 
-//Zadanie 3
+// Z A D A N I E  3
 console.log("Hello World");
 //console.log(Hello World); // bład, nieznana zmienna
 // console.log("Hello World); //bład, niezakończony string
