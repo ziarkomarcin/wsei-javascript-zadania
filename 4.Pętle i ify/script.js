3fa19e9fe7a1fc476b0895cb2755498fb1ede777
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-11-22 09:24:20 +0100

    done

--- 4.Pętle i ify/script.js
+++ 4.Pętle i ify/script.js
@@ -101,3 +101,51 @@ for(let x = 0; x < 5; x++) {
  }
  console.log(result);
  }
+
+// Z A D A N I E  8.c
+
+  let tree = '';
+  const height = 5;
+  let stars = 1;
+  for (let row = 1; row <= height; row++) {
+    tree += ' '.repeat(height-row);
+    tree += '*'.repeat(stars) + '\n';
+    stars += 2;
+  }
+  console.log(tree);
+
+// Z A D A N I E  8.d
+
+let show = '';
+  let show1 = '';
+  const height = 5;
+  for (let row = 1; row <= height; row++) {
+    show += '*'.repeat(row)
+    for (let i = row; i < height; i++) {
+      show += i;
+    }
+    show += '\n';
+
+    show1 += '*'.repeat(height - row + 1);
+    for (let i = (height - row + 1); i < height; i++) {
+      show1 += i;
+    }
+    show1 += '\n';
+  }
+  console.log(show);
+  console.log("-".repeat(height));
+  console.log(show1);
+
+// Z A D A N I E  8.e
+
+  var dart = '';
+  const size = 5;
+  for (let i = 1; i <= size; i++) {
+    dart += ' '.repeat(size-i);
+    dart += '* '.repeat(i) + '\n';
+  }
+  for (let i = 0; i < (size-2); i++) {
+    dart += " ".repeat(size-1);
+    dart += "* \n";
+  }
+  console.log(dart);
