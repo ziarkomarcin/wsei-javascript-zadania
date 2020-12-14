9258f2b2a516b559c4e1a30d0c300e110083983e
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-12-14 22:30:57 +0100

    something

---
+++ 14.DOM-elementy/script.js
@@ -0,0 +1,25 @@
+// Z A D A N I E  1
+
+document.addEventListener('DOMContentLoaded', () => {
+  var tagsTab = [];
+    document.querySelectorAll('.more-divs').forEach((element) => {
+        if (!tagsTab.includes(element.tagName)) {
+            tagsTab.push(element.tagName);
+        }
+    });
+    console.log(tagsTab);
+    
+// Z A D A N I E  2
+  
+  function display(a) {
+        console.log(a.innerHTML);
+        console.log(a.outerHTML);
+        console.log([...a.classList].join(''));
+        console.log([...a.classList]);
+        console.log(a.dataset)
+    }
+
+    display(document.querySelector('.short-list'));    
+    
+    
+});
