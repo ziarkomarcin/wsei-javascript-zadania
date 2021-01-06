8de1e9e175bb859595e756ec2bceef2dea3aadd8
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-01-06 18:19:41 +0100

    first

---
+++ 15.DOM-Tworzenie elementów/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+// Z A D A N I E  1
+
+var exe = document.querySelector('#root');
+  var newElem = document.createElement('div');
+  newElem.innerHTML = 'To jest nowy element';
+  exe.appendChild(newElem);
+  
+  
+
+});
