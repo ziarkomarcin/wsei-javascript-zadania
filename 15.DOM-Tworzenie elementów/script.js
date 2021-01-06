751e89eb224a5868d855845106df2703a24b3818
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-01-06 19:33:07 +0100

    ... - - - 6 is the next - - - ...

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -9,30 +9,48 @@ var exe = document.querySelector('#root');
   
  // Z A D A N I E  2
   
-  var ol = document.createElement('ol');
+  const myTab = ["Cherry", "Orange", "Peach", "Apple", "Watermelon", "Sea Buckthorn"];
+  const myList = document.createElement('ol');
+  myList.id = 'myList';
 
-  var first = document.createElement('li');
-  first.innerText = "Cherry";
-  var second = document.createElement('li');
-  second.innerText = "Orange";
-  var third = document.createElement('li');
-  third.innerText = "Peach";
-  var fourth = document.createElement('li');
-  fourth.innerText = "Apple";
-  var fifth = document.createElement('li');
-  fifth.innerText = "Watermelon";
-  var sixth = document.createElement('li');
-  sixth.innerText = "Sea Buckthorn";
+myTab.forEach((a) => {
+    var myFruit = document.createElement('li');
+    myFruit.innerText = a;
 
-  exe.appendChild(ol);
-  ol.appendChild(first);
-  ol.appendChild(second);
-  ol.appendChild(third);
-  ol.appendChild(fourth);
-  ol.appendChild(fifth);
-  ol.appendChild(sixth);
+    myList.append(myFruit);
+});
+
+newElem.append(myList);
+document.body.insertBefore(newElem, document.getElementById('root'));  
   
   // Z A D A N I E  3
+  
+  newElem.addEventListener('click', () => {
+    Array.from(myList.children).forEach((el, x) => {
+        if (x % 2 === 1) {
+            myList.removeChild(el);
+        }
+    });
+});
+  
+    // Z A D A N I E  4
+  
+   var btn = document.createElement('button');
+  btn.innerText = 'Click&Hide';
+  btn.addEventListener('click', (event) => event.target.remove());
+  exe.appendChild(btn);
+  
+  // Z A D A N I E  5
+  
+  var random = Math.round(Math.random() * 10);
+
+  for (var a = 0; a < random; a++) {
+      var someDiv = document.createElement('div');
+
+      someDiv.innerHTML = `to jest div numer ${a}`;
+      exe.appendChild(someDiv);
+  }
+  
   
 
 });
