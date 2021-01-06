676462fdb25c0543c69c7928adecb06a9ecd99fa
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-01-06 18:32:37 +0100

    second

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -7,6 +7,32 @@ var exe = document.querySelector('#root');
   newElem.innerHTML = 'To jest nowy element';
   exe.appendChild(newElem);
   
+ // Z A D A N I E  2
+  
+  var ol = document.createElement('ol');
+
+  var first = document.createElement('li');
+  first.innerText = "Cherry";
+  var second = document.createElement('li');
+  second.innerText = "Orange";
+  var third = document.createElement('li');
+  third.innerText = "Peach";
+  var fourth = document.createElement('li');
+  fourth.innerText = "Apple";
+  var fifth = document.createElement('li');
+  fifth.innerText = "Watermelon";
+  var sixth = document.createElement('li');
+  sixth.innerText = "Sea Buckthorn";
+
+  exe.appendChild(ol);
+  ol.appendChild(first);
+  ol.appendChild(second);
+  ol.appendChild(third);
+  ol.appendChild(fourth);
+  ol.appendChild(fifth);
+  ol.appendChild(sixth);
+  
+  // Z A D A N I E  3
   
 
 });
